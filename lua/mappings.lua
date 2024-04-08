91b531d4af5b9fd39eba1758c01552a224444616
Paul Shuker <paul.shuker@outlook.com>
2024-04-08 18:40:11 +0100

    new key mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,33 +6,12 @@ map("i", "jk", "<ESC>")
 
 local terminal = require "nvchad.term"
 -- toggle one horizontal terminal window by alt + h in normal or terminal mode
-map({ "n", "t" }, "<A-h>", function()
-  terminal.toggle { pos = "sp", id = "toggleh" }
+map({ "n", "t" }, "<C-h>", function()
+  terminal.toggle { pos = "vsp", id = "toggleterm" }
 end)
--- toggle one horizontal terminal window by alt + h
-map({ "n", "t" }, "<A-v>", function()
-  terminal.toggle { pos = "vsp", id = "togglev" }
-end)
--- run python script in toggleable terminal on the current file
-map({ "n", "t" }, "<A-r>", function()
+map({ "n", "t" }, "<C-e>", function()
   terminal.runner {
-    id = "toggleh",
-    pos = "sp",
-
-    cmd = function()
-      local file = vim.fn.expand "%"
-
-      local ft_cmds = {
-        python = "python3 " .. file,
-      }
-
-      return ft_cmds[vim.bo.ft]
-    end,
-  }
-end)
-map({ "n", "t" }, "<A-t>", function()
-  terminal.runner {
-    id = "togglev",
+    id = "toggleterm",
     pos = "vsp",
 
     cmd = function()
